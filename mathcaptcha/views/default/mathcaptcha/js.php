ac5fdc40337d290cd634b5cfbdba4707dadef5dd
Saket Saurabh <tachyon.saket@gmail.com>
2011-03-27 16:40:49 +0530

    updating

--- mathcaptcha/views/default/mathcaptcha/js.php
+++ mathcaptcha/views/default/mathcaptcha/js.php
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 		$.ajax({
-			url: "<?php echo $vars['url'] . "mathcaptcha/$token"; ?>" + $('#captcha_token').val(),
+			url: "<?php echo $vars['url'] . "pg/mathcaptcha/$token"; ?>" + $('#captcha_token').val(),
 			dataType: 'json',
 			success: function(data){
 			$('#mathcaptacha').html(data.q);
