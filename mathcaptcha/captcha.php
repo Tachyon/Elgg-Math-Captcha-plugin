4cdaa9823f7b18bef19e2873cbf96b332c6ad963
Saket Saurabh <tachyon.saket@gmail.com>
2011-03-28 17:15:04 +0530

    updated with correct coding format

--- mathcaptcha/captcha.php
+++ mathcaptcha/captcha.php
@@ -1,34 +1,28 @@
 <?php
-	/**
-	 * Elgg mathcaptcha plugin quetion page generator
-	 * 
-	 * @package ElggMathCaptcha
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Saket Saurabh
-	 * @copyright Saket Saurabh 2011
-	 * @link http://elgg.com/
-	 */
+/**
+ * Elgg mathcaptcha plugin question page generator
+ * 
+ * @package ElggMathCaptcha
+ * @author Saket Saurabh
+ */
 
-	global $CONFIG;
-	$token = get_input('captcha_token');
-	
-	$json = array();
-	$json['success'] = false;
-	
-	if ($token)
-	{ 
-		// Get the question
-		$question = captcha_get_question(captcha_generate_captcha($token));
-			
-		// Create the array to retruned
-		$json['success']=true;
-		$json['q']=$question[0];		
-	}
-	else
-	{
-		$json['q']=elgg_echo('captcha:errorloading');
-	}
-	
-	// Output the quetion as a valid JSON object
-	echo json_encode($json);
-?>
+global $CONFIG;
+$token = get_input('captcha_token');
+
+$json = array();
+$json['success'] = false;
+
+if ($token) { 
+	// Get the question
+	$question = captcha_get_question(captcha_generate_captcha($token));
+		
+	// Create the array to retruned
+	$json['success']=true;
+	$json['q']=$question[0];		
+}
+else {
+	$json['q']=elgg_echo('captcha:errorloading');
+}
+
+// Output the quetion as a valid JSON object
+echo json_encode($json);
--- mathcaptcha/views/default/input/captcha.php
+++ mathcaptcha/views/default/input/captcha.php
@@ -1,16 +1,13 @@
 <?php
-	/**
-	 * Elgg mathcaptcha plugin captcha hook view override.
-	 * 
-	 * @package ElggMathCaptcha
-	 * @license http://www.gnu.org/licenses/old-licenses/gpl-2.0.html GNU Public License version 2
-	 * @author Saket Saurabh
-	 * @copyright Saket Saurabh 2011
-	 * @link http://elgg.com/
-	 */
+/**
+ * Elgg mathcaptcha plugin captcha hook view override.
+ * 
+ * @package ElggMathCaptcha
+ * @author Saket Saurabh
+ */
 
-	// Generate a token which is then passed into the captcha algorithm for verification
-	$token = captcha_generate_token();
+// Generate a token which is then passed into the captcha algorithm for verification
+$token = captcha_generate_token();
 ?>
 	
 <div class="captcha">
